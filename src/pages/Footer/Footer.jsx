23759aa2e9563c75bd4a1502743fd51af4a81e4f
timothy-okoduwa <timmyleeokoduwa7@gmail.com>
2023-05-05 14:03:54 +0100

    vanilaa

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -1,27 +1,98 @@
-import React from 'react'
+import React from 'react';
 import './Footer.css';
+import v from '../images/vp.svg';
+import { Bounce } from 'react-awesome-reveal';
 const Footer = () => {
   return (
-    <div className="container">
-      <div className="promises">
-        <div>
-          <div className="row">
-            <div className="col-12 col-lg-7">
-              <div className="sched">
-                Schedule time to <span className="know">talk with us.</span>
+    <div className="grey">
+      <div className="container">
+        <div className="promises">
+          <div>
+            <div className="row">
+              <div className="col-12 col-lg-7">
+                <Bounce duration="800" triggerOnce="true" direction="down">
+                  <div>
+                    <div className="sched">
+                      Schedule time to{' '}
+                      <span className="know">talk with us.</span>
+                    </div>
+                    <div className="letss">Let’s talk about your goals</div>
+                    <div className="reje">
+                      <button className="magnify">
+                        Book 30 Minutes Session
+                      </button>
+                      <button className="capmi">Contact Us</button>
+                    </div>
+                  </div>
+                </Bounce>
               </div>
-              <div className="letss">Let’s talk about your goals</div>
-              <div className="reje">
-                <button className="magnify">Book 30 Minutes Session</button>
-                <button className="capmi">Contact Us</button>
+              <div className="col-12 col-lg-5"></div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="wituyt">
+            <div className="row">
+              <div className="col-12 col-lg-4">
+                <div>
+                  <div>
+                    <img src={v} alt="" />
+                    <div className="collab">
+                      Let's collaborate! Hire our team to build
+                      <br /> amazing, user friendly products for
+                      <br /> your business.
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-12 col-lg-3">
+                <div>
+                  <div className="comppp">Company</div>
+                  <div className="wghat">What we do</div>
+                  <div className="wghat">About Us</div>
+                </div>
+              </div>
+              <div className="col-12 col-lg-3">
+                <div>
+                  <div className="comppp">Resources</div>
+                  <div className="wghat">Projects</div>
+                  <div className="wghat">Products</div>
+                </div>
+              </div>
+              <div className="col-12 col-lg-2">
+                <div>
+                  <div className="comppp">Media</div>
+                  <div className="wghat">Contact us</div>
+                  <div className="wghat">Blog</div>
+                </div>
               </div>
             </div>
-            <div className="col-12 col-lg-5"></div>
+            <div className="row">
+              <div className="col">
+                <div className="dribb">Dribbble</div>
+              </div>
+              <div className="col">
+                <div className="dribb">Behance</div>
+              </div>
+              <div className="col">
+                <div className="dribb">Linkedin</div>
+              </div>
+              <div className="col">
+                <div className="dribb">Instagram</div>
+              </div>
+              <div className="col">
+                <div className="dribb">Facebook</div>
+              </div>
+            </div>
+
+            <div>
+              <div className="f"></div>
+            </div>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Footer
+export default Footer;
--- src/pages/HomePage/Building.jsx
+++ src/pages/HomePage/Building.jsx
@@ -3,11 +3,12 @@ import r from '../images/rocky.png';
 import f from '../images/fin.png';
 import l from '../images/finacery logo.svg';
 import s from '../images/scl.svg';
-import u from '../images/ugat.png'
-import b from '../images/booklib.svg'
-import p from '../images/pstt.png'
-import cc from '../images/crypto.svg'
-import oo from '../images/coin.png'
+import u from '../images/ugat.png';
+import b from '../images/booklib.svg';
+import p from '../images/pstt.png';
+import cc from '../images/crypto.svg';
+import oo from '../images/coin.png';
+import { Zoom } from 'react-awesome-reveal';
 
 const Building = () => {
   return (
@@ -31,142 +32,146 @@ const Building = () => {
       </div>
 
       <div className="takes">
-        <div className="dor">
-          <div className="waiting-for">
-            <div className="containerr-fluid">
-              <div className="row mt-5">
-                <div className="col-12 col-lg-5">
-                  <div className="tickets">
-                    <div>
+        <Zoom duration="800" triggerOnce="true">
+          <div className="dor">
+            <div className="waiting-for">
+              <div className="containerr-fluid">
+                <div className="row mt-5">
+                  <div className="col-12 col-lg-5">
+                    <div className="tickets">
                       <div>
-                        <img src={l} alt="" style={{ width: '35%' }} />
-                      </div>
-                      <div className="startl">
-                        Start a ride hailing business, or Logistics startup
-                        today. Let's build your product from start to finish, go
-                        to market in 8 weeks.
-                      </div>
-                      <div className="u-alone">
-                        <div className="presence">Web Development</div>
-                        <div className="presence2">UI/UX Design</div>
-                        <div className="presence3">Branding </div>
-                      </div>
-                      <div>
-                        <button className="sbb">Start Building</button>
+                        <div>
+                          <img src={l} alt="" style={{ width: '35%' }} />
+                        </div>
+                        <div className="startl">
+                          Financery Dashboard- Manage all your finance, crypto
+                          and stock all from one place.
+                        </div>
+                        <div className="u-alone">
+                          <div className="presence">UI/UX Design</div>
+                          <div className="presence2">Branding</div>
+                        </div>
+                        <div>
+                          <button className="sbb">Start Building</button>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-                <div className="col-12 col-lg-7">
-                  <div>
-                    <img src={f} alt="" className="nono" />
+                  <div className="col-12 col-lg-7">
+                    <div>
+                      <img src={f} alt="" className="nono" />
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        </Zoom>
+
         <div className="caving">
           <div className="row">
             <div className="col-12 col-lg-6 mb-5">
-              <div className="loast">
-                <div className="lplp">
-                  <div className="ynl">
-                    <div>
-                      <img src={s} alt=""  />
-                     
-                    </div>
-                    <div className="memind">
-                      Start a ride hailing business, or Logistics startup today.
-                      Let's build your product from start to finish, go to
-                      market in 8 weeks.
-                    </div>
-                    <div>
-                      <div className="u-alone">
-                        <div className="presence">Web Development</div>
-                        <div className="presence2">UI/UX Design</div>
-                        <div className="presence3">Branding </div>
+              <Zoom duration="800" triggerOnce="true">
+                <div className="loast">
+                  <div className="lplp">
+                    <div className="ynl">
+                      <div>
+                        <img src={s} alt="" />
+                      </div>
+                      <div className="memind">
+                        An E-learning platform for all ages.
                       </div>
                       <div>
-                        <button className="sbbb">Start Building</button>
+                        <div className="u-alone">
+                          <div className="presence">UI/UX Design</div>
+                          <div className="presence2">Branding</div>
+                        </div>
+                        <div>
+                          <button className="sbbb mb-4">Start Building</button>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-                <div>
                   <div>
-                    <img src={u} alt="" style={{ width: '100%' }} />
+                    <div>
+                      <img src={u} alt="" style={{ width: '100%' }} />
+                    </div>
                   </div>
                 </div>
-              </div>
+              </Zoom>
             </div>
+
             <div className="col-12 col-lg-6 mb-5">
-              <div className="loast2">
-                <div className="lplp">
-                  <div className="ynl">
-                    <div>
-                      <img src={b} alt=""  />
-                    </div>
-                    <div className="memind">
-                      An online library with inspirationals books from famous
-                      achievers
-                    </div>
-                    <div>
-                      <div className="u-alone">
-                        <div className="presence">Web Development</div>
-                        <div className="presence2">UI/UX Design</div>
-                        <div className="presence3">Branding </div>
+              <Zoom duration="800" triggerOnce="true">
+                <div className="loast2">
+                  <div className="lplp">
+                    <div className="ynl">
+                      <div>
+                        <img src={b} alt="" />
+                      </div>
+                      <div className="memind">
+                        An online library with inspirationals books from famous
+                        achievers
                       </div>
                       <div>
-                        <button className="sbbb2 mb-4">Start Building</button>
+                        <div className="u-alone">
+                          <div className="presence">UI/UX Design</div>
+                          <div className="presence2">Branding</div>
+                          {/* <div className="presence3"> </div> */}
+                        </div>
+                        <div>
+                          <button className="sbbb2 ">Start Building</button>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-                <div>
                   <div>
-                    <img src={p} alt="" style={{ width: '96%' }} />
+                    <div>
+                      <img src={p} alt="" style={{ width: '96%' }} />
+                    </div>
+                  </div>
+                </div>
+              </Zoom>
+            </div>
+          </div>
+        </div>
+
+        <Zoom duration="800" triggerOnce="true">
+          <div className="dor">
+            <div className="waiting-for2">
+              <div className="containerr-fluid">
+                <div className="row mt-5">
+                  <div className="col-12 col-lg-5">
+                    <div className="tickets">
+                      <div>
+                        <div>
+                          <img src={cc} alt="" style={{ width: '50%' }} />
+                        </div>
+                        <div className="startl">
+                          Start a ride hailing business, or Logistics startup
+                          today. Let's build your product from start to finish,
+                          go to market in 8 weeks.
+                        </div>
+                        <div className="u-alone">
+                          <div className="presence">UI/UX Design</div>
+                          <div className="presence2">Branding</div>
+                        </div>
+                        <div>
+                          <button className="sbb2p">Start Building</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-12 col-lg-7">
+                    <div>
+                      <img src={oo} alt="" className="nono" />
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="dor">
-          <div className="waiting-for2">
-            <div className="containerr-fluid">
-              <div className="row mt-5">
-                <div className="col-12 col-lg-5">
-                  <div className="tickets">
-                    <div>
-                      <div>
-                        <img src={cc} alt="" style={{ width: '50%' }} />
-                      </div>
-                      <div className="startl">
-                        Start a ride hailing business, or Logistics startup
-                        today. Let's build your product from start to finish, go
-                        to market in 8 weeks.
-                      </div>
-                      <div className="u-alone">
-                        <div className="presence">Web Development</div>
-                        <div className="presence2">UI/UX Design</div>
-                        <div className="presence3">Branding </div>
-                      </div>
-                      <div>
-                        <button className="sbb2p">Start Building</button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-12 col-lg-7">
-                  <div>
-                    <img src={oo} alt="" className="nono" />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        </Zoom>
       </div>
     </div>
   );
