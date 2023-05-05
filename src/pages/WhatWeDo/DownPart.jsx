115da46df20ae0bb9ad7c6d2b2f053ac757605c5
timothy-okoduwa <timmyleeokoduwa7@gmail.com>
2023-05-05 17:54:39 +0100

    sokoto

--- src/pages/WhatWeDo/DownPart.jsx
+++ src/pages/WhatWeDo/DownPart.jsx
@@ -4,8 +4,12 @@ import s from '../images/screen.svg';
 import m from '../images/mobile.svg';
 import w from '../images/web.svg';
 import { Zoom } from 'react-awesome-reveal';
-import r from '../images/rocky.png'
-import d from '../images/doller.svg'
+import r from '../images/rocky.png';
+import d from '../images/doller.svg';
+import bit from '../images/bitcoin.svg';
+import wb from '../images/webb.svg';
+import ecom from '../images/ecc.svg';
+import edu from '../images/eduuu.svg';
 const DownPart = () => {
   return (
     <div className="sby">
@@ -111,23 +115,101 @@ const DownPart = () => {
         </div>
         <div className="mkir">
           <div className="row">
-            <div className="col">
-              <div className="fullterms">
-                <div className="containerd">
-                  <div>
+            <div className="col-12 col-lg-6 mb-5">
+              <Zoom duration="800" triggerOnce="true">
+                <div className="fullterms">
+                  <div className="containerd">
                     <div>
-                      <img src={d} alt="" />
-                    </div>
-                    <div className="brandd">Fintech</div>
-                    <div className="whiyr">
-                      With good branding, you can give your<br/> brand a more human
-                      side, to make <br/>your business stand out.
+                      <div>
+                        <img src={d} alt="" />
+                      </div>
+                      <div className="Fintech">Fintech</div>
+                      <div className="goof">
+                        With good branding, you can give your
+                        <br /> brand a more human side, to make <br />
+                        your business stand out.
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
+              </Zoom>
             </div>
-            <div className="col">ruru</div>
+            <div className="col-12 col-lg-6 mb-5">
+              <Zoom duration="800" triggerOnce="true">
+                <div className="fullterms2">
+                  <div className="containerd">
+                    <div>
+                      <div>
+                        <img src={bit} alt="" />
+                      </div>
+                      <div className="crypto">Crypto & Web3</div>
+                      <div className="humm">
+                        We design human centered products
+                        <br /> to attract audience and make
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Zoom>
+            </div>
+            <div className="col-12 col-lg-6 mb-5">
+              <Zoom duration="800" triggerOnce="true">
+                <div className="fullterms3">
+                  <div className="containerd">
+                    <div>
+                      <div>
+                        <img src={wb} alt="" />
+                      </div>
+                      <div className="website">Websites</div>
+                      <div className="nomind">
+                        We help your business to build a<br /> web application
+                        that serves <br />
+                        customers/ service needs
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Zoom>
+            </div>
+            <div className="col-12 col-lg-6 mb-5">
+              <Zoom duration="800" triggerOnce="true">
+                <div className="fullterms4">
+                  <div className="containerd">
+                    <div>
+                      <div>
+                        <img src={ecom} alt="" />
+                      </div>
+                      <div className="ecoo">E-Commerce</div>
+                      <div className="waitfor">
+                        We help your business to build a web <br />
+                        application that serves customers/ <br />
+                        service needs, increase business growth
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Zoom>
+            </div>
+            <div className="col-12 col-lg-6 mb-5">
+              <Zoom duration="800" triggerOnce="true">
+                <div className="fullterms5">
+                  <div className="containerd">
+                    <div>
+                      <div>
+                        <img src={edu} alt="" />
+                      </div>
+                      <div className="edu">EduTech</div>
+                      <div className="die">
+                        We help you develop mobile
+                        <br /> application that give your users
+                        <br /> awesome experience and generate <br />
+                        leads, and increase sales
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Zoom>
+            </div>
           </div>
         </div>
       </div>
