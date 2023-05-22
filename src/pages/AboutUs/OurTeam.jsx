9f144f718a17182bd8ec28eb62fc8698df7051ff
timothy-okoduwa <timmyleeokoduwa7@gmail.com>
2023-05-22 15:39:01 +0100

    10 toes

--- src/pages/AboutUs/OurTeam.jsx
+++ src/pages/AboutUs/OurTeam.jsx
@@ -3,7 +3,7 @@ import b from '../images/bosspics.png';
 import c from '../images/chinexpics.png';
 import d from '../images/debbypics.png';
 import e from '../images/timpics.png';
-import f from '../images/blesspics.png';
+import f from '../images/news.png';
 import g from '../images/judepics.png';
 const OurTeam = () => {
   return (
@@ -11,7 +11,7 @@ const OurTeam = () => {
       <div>
         <div className="ourR">
           {' '}
-          Our <span className='gdgd'>Team</span>
+          Our <span className="gdgd">Team</span>
         </div>
         <div className=" gettoknow">GET TO KNOW</div>
       </div>
@@ -22,7 +22,7 @@ const OurTeam = () => {
             <div className="col-12 col-lg-4 mb-5 heyye">
               <div>
                 <div className="wawawa ">
-                  <img src={b} alt="" className="band" />
+                  <img src={b} alt="images" className="band" />
                 </div>
                 <div className="slyy">Sylvester Nnachi</div>
                 <div className="product">Product Designer</div>
@@ -31,7 +31,7 @@ const OurTeam = () => {
             <div className="col-12 col-lg-4 mb-5 d-flex justify-content-center">
               <div>
                 <div className="wawawa">
-                  <img src={c} alt="" className="band" />
+                  <img src={c} alt="images" className="band" />
                 </div>
                 <div className="slyy">Chinedu Ogbonna</div>
                 <div className="product">Digital Marketer</div>
@@ -40,7 +40,7 @@ const OurTeam = () => {
             <div className="col-12 col-lg-4 mb-5 starte">
               <div>
                 <div className="wawawa ">
-                  <img src={d} alt="" className="band" />
+                  <img src={d} alt="images" className="band" />
                 </div>
                 <div className="slyy">Deborah Ehru</div>
                 <div className="product">UI/UX Designer</div>
@@ -49,7 +49,7 @@ const OurTeam = () => {
             <div className="col-12 col-lg-4 mb-5 d-flex heyye">
               <div>
                 <div className="wawawa">
-                  <img src={e} alt="" className="band" />
+                  <img src={e} alt="images" className="band" />
                 </div>
                 <div className="slyy">Timothy Okoduwa</div>
                 <div className="product">Frontend Developer</div>
@@ -58,16 +58,16 @@ const OurTeam = () => {
             <div className="col-12 col-lg-4 mb-5 d-flex justify-content-center">
               <div>
                 <div className="wawawa ">
-                  <img src='' alt="" className="band" />
+                  <img src={f} alt="images" className="band" />
                 </div>
-                <div className="slyy">Blessing Ogbonna</div>
-                <div className="product">Graphics Designer</div>
+                <div className="slyy">Peace Ukaoha</div>
+                <div className="product">Product Manager</div>
               </div>
             </div>
             <div className="col-12 col-lg-4 mb-5 starte">
               <div>
                 <div className="wawawa">
-                  <img src={g} alt="" className="band" />
+                  <img src={g} alt="images" className="band" />
                 </div>
                 <div className="slyy">Jude Nwosu</div>
                 <div className="product">Graphics Designer</div>
